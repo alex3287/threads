664544a7aedc4948ed7d867f33316e99fca20541
Alexander Mangazeev <alex3287@bk.ru>
2021-03-20 23:41:25 +0700

    try create queue

--- laba_2/queue.cpp
+++ laba_2/queue.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "queue.h"
+
+Queue::Queue() {
+    size = 0;
+    first = nullptr;
+    last = nullptr;
+}
+
+Queue::~Queue() {
+//    for (int i=0; i<size; i++){ fixme
+//
+//    }
+//    for(auto it=item;it;it = it->next)...
+}
+
+bool Queue::isEmpty() {
+    if (size)
+        return false;
+    return true;
+}
+
+void Queue::push(int tempDate) {
+    size++;
+    if (size == 1){
+        first = new node;
+        first->data = tempDate;
+        last = first;
+    } else {
+        node *tmp = new node;
+        tmp->next = last;
+        last->next = tmp;
+//        last->next = tmp;
+//        last = tmp;  TODO тут возник спор
+    }
+}
+//ctrl+shift+d почему не используешь
+
+// TODO опасность
+int Queue::pop() {
+    int tmp = first->data;
+    size--;
+
+    return tmp;
+}
+
+
--- laba_2/queue.h
+++ laba_2/queue.h
@@ -2,15 +2,29 @@
 // Created by Александр Мангазеев on 16.03.2021.
 //
 
+// first -> f <- s <- t <- f <- last
+
 #ifndef LABA_2_QUEUE_H
 #define LABA_2_QUEUE_H
 
-
-
-class queue {
-
+struct node{
+    int data;
+    node *next;
 };
 
+class Queue {
+public:
+    Queue();
+    ~Queue();
+    bool isEmpty();
+    void push(int tempData);
+    int pop();
+
+private:
+    int size;
+    node *first;
+    node *last;
+};
 
 
 #endif //LABA_2_QUEUE_H
